a25ab33193a19bf905a7852c75f0d6ee5a9f2f99
RobbieMurray <robbodym@gmail.com>
2020-10-25 21:40:43 +0000

    Added env

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
+const PORT = process.env.PORT || 5000;
+
+//env variables
 
 //middlware
 app.use(cors());
 app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  //server static content
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
 
 //Routes//
 
@@ -70,5 +78,5 @@ app.delete("/todos/:id", async (req, res) => {
 });
 
 app.listen(5000, () => {
-  console.log("server has started on port 5000");
+  console.log(`server has started on port ${PORT}`);
 });
